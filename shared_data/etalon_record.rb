00c2bdde3f88bbc7eece82ea90525d0896648270
Mopc <mopc@roboterwerke.su>
2021-02-05 00:55:08 +0200

    some new classes anable definition of PLANK_CONSTANT and ELECTRIC_FIELD_CONTANT

--- shared_data/etalon_record.rb
+++ shared_data/etalon_record.rb
@@ -18,38 +18,3 @@ end
 # 8.85418782 × 10-12 m-3 kg-1 s4 A2
 
 
-
-
-# following is usless for now
-
-# this class defines one record
-class EtalonRecord
-    # initialize is a constructor method
-    def initialize(name, param1, param2)
-        # attribute definition and value assignment
-        @name = name
-        @mass = param1
-        @length = param2
-    end
-end
-
-# this class has an array of data
-# and a method for data
-class DataGenerator
-    # define data in this method
-    @arr = Array.new # a list of elements (actually an array, but lets call it a list)
-    def define_data
-        # create billion billion billion billion elements
-        @arr.push(EtalonRecord.new("MassUnit",g,gram))
-        @arr.push(EtalonRecord.new("LengthUnit",m,meter))
-        # @arr.push(EtalonRecord.new("MassConstant",0.00000000000000000000000000167262))
-        # @arr.push(EtalonRecord.new("ElectronMass",1 x "MassConstant"))
-        # @arr.push(EtalonRecord.new("ProtonMass",1836 x "ElectronMass"))
-        # @arr.push(EtalonRecord.new("ElectronDiameter",000000000000000565))
-        # @arr.push(EtalonRecord.new("ProtonDiameter",0000000000000000833))
-    end
-
-    def write_json
-        # todo
-    end
-end
--- shared_data/si_unit.rb
+++ shared_data/si_unit.rb
@@ -2,10 +2,10 @@
 class SiUnit
     # defines one value.
     # second, meter, etc. are defined as powers
-    def initialize(val, s, m, kg, A, K, mon, cd)
-        # attribute definition and value assignment
-        @value = val
-        @second = s
+    # example: Joule is kg⋅m^2⋅s^−2 => kg = 1, m = 2, s = -2, A = 0, K = 0, mon = 0, cd = 0
+    def initialize(s, m, kg, A, K, mon, cd)
+        # powers as si units
+        @second = s # example: 2 for s², -2 for 1/s²
         @metre = m
         @kilogram = kg
         @ampere = A
@@ -15,21 +15,78 @@ class SiUnit
     end
 end
 
-class Power
+class PowerValue
     # 6.62607015×10^−34 is (6.62607015, -34)
     def initialize(value, power)
-        @value = value
-        @power = power
+        @value = value # float
+        @power = power # int
+    end
+
+    def calculate
+        if @power == 1
+            return @value
+        else
+            return @value * 10 ** @power
+    end
+end
+
+class SiValue
+    def initialize(powered_value, si_unit)
+        # value as PowerValue class: value ^ power
+        @value = powered_value
+        # unit as SiUnit class
+        @unit = si_unit
+    end
+end
+
+# derived units return si unit powers
+class DerivedUnits
+    # s^p
+    def Second(p)
+        return SiUnit.new(p, 0, 0, 0, 0, 0, 0)
+    end
+    # m^p
+    def Meter(p)
+        return SiUnit.new(0, p, 0, 0, 0, 0, 0)
+    end
+    # kg^p
+    def Kilogram(p)
+        return SiUnit.new(0, 0, p, 0, 0, 0, 0)
+    end
+    # A^p
+    def Ampere(p)
+        return SiUnit.new(0, 0, 0, p, 0, 0, 0)
+    end
+    # K^p
+    def Kelvin(p)
+        return SiUnit.new(0, 0, 0, 0, p, 0, 0)
+    end
+    # mol^p
+    def Mole(p)
+        return SiUnit.new(0, 0, 0, 0, 0, p, 0)
+    end
+    # cd^p
+    def Candela(p)
+        return SiUnit.new(0, 0, 0, 0, 0, 0, p)
+    end
+    # https://en.wikipedia.org/wiki/Joule
+    def Joule
+        return Kilogram(1) * Meter(2) * Second(-2)
     end
 end
 
 # example Planck constant. in J*s = kg * m^2 * s^-2 * s = kg * m^2 * s^-1
-plank_contant = SiUnit.new(
-    Power.new(6.62607015, -34), # 6.62607015 * 10^-34
-    -1, # s (J*s => s^-2*s = s^1) 
-    2, # m
-    1, # kg
-    0, # A
-    0, # K
-    0, # mol
-    0) # cd
+PLANK_CONSTANT = SiValue.new(
+    PowerValue.new(6.62607015, -34), # 6.62607015 * 10^-34
+    DerivedUnits.Joule(1) * DerivedUnits.Second(1)
+)
+
+# Electric field constant should bi like this
+# 8.85418782 × 10-12 m-3 kg-1 s4 A2
+ELECTRIC_FIELD_CONTANT = SiValue.new(
+    PowerValue.new(8.85418782, -12), # 8.85418782 × 10-12
+    DerivedUnits.Meter(-3) # m^-3
+        * DerivedUnits.Kilogram(-1) # kg^-1
+        * DerivedUnits.Second(4) # s^4
+        * DerivedUnits.Ampere(2) # A^2
+)
