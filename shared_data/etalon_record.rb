ef803c1a763aff4443cbd7918aca29fe19fa3931
Mopc <mopc@roboterwerke.su>
2021-02-04 22:08:29 +0200

    changed to machine understandable

--- shared_data/etalon_record.rb
+++ shared_data/etalon_record.rb
@@ -1,11 +1,35 @@
+# constants definitions
+
+MASS_CONSTANT 0.00000000000000000000000000167262
+ELECTRON_DIAMETER 0.00000000000000565
+PROTOM_DIAMETER 0.00000000000000833
+
+# calculations
+
+def ElectronMass
+  return 1 * MASS_CONSTANT
+end
+
+def ProtonMass
+  return ElectronMass * 1836
+end
+
+# Electric field constant should bi like this
+# 8.85418782 × 10-12 m-3 kg-1 s4 A2
+
+
+
+
+# following is usless for now
+
 # this class defines one record
 class EtalonRecord
     # initialize is a constructor method
     def initialize(name, param1, param2)
         # attribute definition and value assignment
         @name = name
-        @param1 = param1
-        @param2 = param2
+        @mass = param1
+        @length = param2
     end
 end
 
@@ -18,11 +42,11 @@ class DataGenerator
         # create billion billion billion billion elements
         @arr.push(EtalonRecord.new("MassUnit",g,gram))
         @arr.push(EtalonRecord.new("LengthUnit",m,meter))
-        @arr.push(EtalonRecord.new("MassConstant",0.00000000000000000000000000167262))
-        @arr.push(EtalonRecord.new("ElectronMass",1 x "MassConstant"))
-        @arr.push(EtalonRecord.new("ProtonMass",1836 x "ElectronMass"))
-        @arr.push(EtalonRecord.new("ElectronDiameter",000000000000000565))
-        @arr.push(EtalonRecord.new("ProtonDiameter",0000000000000000833))
+        # @arr.push(EtalonRecord.new("MassConstant",0.00000000000000000000000000167262))
+        # @arr.push(EtalonRecord.new("ElectronMass",1 x "MassConstant"))
+        # @arr.push(EtalonRecord.new("ProtonMass",1836 x "ElectronMass"))
+        # @arr.push(EtalonRecord.new("ElectronDiameter",000000000000000565))
+        # @arr.push(EtalonRecord.new("ProtonDiameter",0000000000000000833))
     end
 
     def write_json
