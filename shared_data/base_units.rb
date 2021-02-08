b646df55a0cbc954d6f18446f09fae4deceda721
h0yer <my.mail.adress.is.longer.than.yours@googlemail.com>
2021-02-08 21:00:03 +0100

    Added area and volume to base units

--- shared_data/base_units.rb
+++ shared_data/base_units.rb
@@ -4,30 +4,38 @@ require_relative 'si_unit'
 class BaseUnits
     # s^p
     def Second(p)
-        return SiUnit.new(p, 0, 0, 0, 0, 0, 0)
+        return SiUnit.new(p, 0, 0, 0, 0, 0, 0 ,0 ,0)
     end
     # m^p
     def Meter(p)
-        return SiUnit.new(0, p, 0, 0, 0, 0, 0)
+        return SiUnit.new(0, p, 0, 0, 0, 0, 0 ,0 ,0)
     end
     # kg^p
     def Kilogram(p)
-        return SiUnit.new(0, 0, p, 0, 0, 0, 0)
+        return SiUnit.new(0, 0, p, 0, 0, 0, 0, 0, 0)
     end
     # A^p -> https://en.wikipedia.org/wiki/Ampere
     def Ampere(p)
-        return SiUnit.new(0, 0, 0, p, 0, 0, 0)
+        return SiUnit.new(0, 0, 0, p, 0, 0, 0, 0, 0)
     end
     # K^p
     def Kelvin(p)
-        return SiUnit.new(0, 0, 0, 0, p, 0, 0)
+        return SiUnit.new(0, 0, 0, 0, p, 0, 0, 0, 0)
     end
     # mol^p
     def Mole(p)
-        return SiUnit.new(0, 0, 0, 0, 0, p, 0)
+        return SiUnit.new(0, 0, 0, 0, 0, p, 0, 0, 0)
     end
     # cd^p
     def Candela(p)
-        return SiUnit.new(0, 0, 0, 0, 0, 0, p)
+        return SiUnit.new(0, 0, 0, 0, 0, 0, p, 0, 0)
+    end
+    # area [m²]
+    def SquareMeter(p)
+        return SiUnit.new(0, 0, 0, 0, 0, 0, 0, p, 0)
+    end
+    # volume [m³]
+    def CubicMeter(p)
+        return SiUnit.new(0, 0, 0, 0, 0, 0, 0, 0, p)
     end
 end
